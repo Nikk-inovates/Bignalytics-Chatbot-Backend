1271cd491be5c1c66d2ba2696ef54e86b75e657c
Nikhil Panchal <nikhilp.ind27@gmail.com>
2025-06-02 19:26:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:8080",
+        "https://chatbot-frontend-seven-lilac.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
